92b2d33c23aaf9af6b94ec6e68ae031f55b081ee
Rimantas <belauzas@gmail.com>
2025-06-19 09:50:40 +0300

    Register validation messages displayed at registration form

--- public/js/register.js
+++ public/js/register.js
@@ -1,6 +1,9 @@
 const formDOM = document.forms[0];
 const usernameDOM = document.getElementById('username');
 const passwordDOM = document.getElementById('password');
+const successDOM = document.getElementById('success_msg');
+const errorUsernameDOM = document.getElementById('error_username');
+const errorPasswordDOM = document.getElementById('error_password');
 
 formDOM.addEventListener('submit', e => {
     e.preventDefault();
@@ -19,7 +22,27 @@ formDOM.addEventListener('submit', e => {
     })
         .then(res => res.json())
         .then(data => {
-            console.log(data);
+            successDOM.classList.remove('info-show');
+            errorUsernameDOM.classList.remove('info-show');
+            errorPasswordDOM.classList.remove('info-show');
+
+            switch (data.status) {
+                case 'success':
+                    successDOM.textContent = data.msg;
+                    successDOM.classList.add('info-show');
+                    break;
+
+                case 'error':
+                    if (data.msg.username) {
+                        errorUsernameDOM.textContent = data.msg.username;
+                        errorUsernameDOM.classList.add('info-show');
+                    }
+                    if (data.msg.password) {
+                        errorPasswordDOM.textContent = data.msg.password;
+                        errorPasswordDOM.classList.add('info-show');
+                    }
+                    break;
+            }
         })
         .catch(err => {
             console.log(err);
